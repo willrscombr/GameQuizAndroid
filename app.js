b9ff535537bb3177775c200e3c73c7963889a83b
Wilton Ribeiro Silva <wilton.tecti@gmail.com>
2016-09-29 17:15:11 -0300

    por hoje é só

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/partidadb');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
--- model/Partida.js
+++ model/Partida.js
@@ -1,3 +1,11 @@
 /**
  * Created by will on 29/09/16.
  */
+var mongoose = require('mongoose');
+
+var PartidaSchema = mongoose.Schema({
+    jogador: String,
+    pontos: Number
+});
+
+mongoose.exports('Partida',PartidaSchema);
