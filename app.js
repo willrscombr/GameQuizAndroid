f8e0a2797318a1b03b74a201d5917d7a3f7c9a3d
Wilton Ribeiro Silva <wilton.tecti@gmail.com>
2016-09-29 14:35:39 -0300

    COnfiguracao rotas iniciais

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/partidas', routes);
+app.use('/partida', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/partidas', function(req, res, next) {
+  res.json({ title: 'Express', id: '1', jogador:'Winton', pontos:'200' });
+});
+
+router.get('/partida', function(req, res, next) {
+  var jogador = req.query.jogador;
+  var pontos = req.query.pontos;
+  res.render('index', { title: 'Express' });
+});
+
+
+
 module.exports = router;
